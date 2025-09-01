7dc38ebc50f7638141b5c732abe026d179cf713a
manjeet-2024 <manjeet.2024@nst.rishihood.edu.in>
2025-09-01 17:12:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+const fs = require("fs").promises;
+
 async function cat(filePaths, outputFilePath) {
-  // write your code here
+  const results = [];
+
+  for (const filePath of filePaths) {
+    try {
+      const stats = await fs.lstat(filePath);
+
+      if (stats.isFile()) {
+        const content = await fs.readFile(filePath, "utf8");
+        results.push(content);
+      } else if (stats.isDirectory()) {
+        results.push("Is a directory");
+      } else {
+        results.push("File not found");
+      }
+    } catch (err) {
+      results.push("File not found");
+    }
+  }
+
+  await fs.writeFile(outputFilePath, results.join("\n"), "utf8");
 }
 
 module.exports = { cat };
